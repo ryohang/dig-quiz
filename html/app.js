aab95b7c06d112f2bf9d3307463511595bdc3a46
Alex van <yell@pituki.com>
2012-12-01 14:54:27 -0500

    stub for send email

--- html/app.js
+++ html/app.js
@@ -33,6 +33,11 @@ app.get('/', function(req, res){
     title: 'Express'
   });
 });
+app.get('/sendemail', function(req, res){
+  res.render('index', {
+    title: 'Express'
+  });
+});
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
