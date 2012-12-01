337595e608ee0401d815e2842db455914d1092e7
Alex van <yell@pituki.com>
2012-12-01 12:39:08 -0500

    added node

---
+++ html/public/js/project.js
@@ -0,0 +1,38 @@
+angular.module('project',[]).
+  config(function($routeProvider) {
+    $routeProvider.
+      when('/', {controller:ListCtrl, templateUrl:'list.html'}).
+      when('/q/:quizId', {controller:TakeQuiz, templateUrl:'detail.html'}).
+      //when('/new', {controller:CreateCtrl, templateUrl:'detail.html'}).
+      otherwise({redirectTo:'/'});
+  }).service('quizData', function(){
+    return [
+      {Label:'quiz 1'
+        ,id:'asdf1'
+        ,questions:[
+        {label:"your name",
+          options:[
+            {label:'alex',correct:false}
+            ,{label:'james',correct:true}
+            ,{label:'peter',correct:false}
+          ]}
+        ,{label:"your name",
+          options:[
+            {label:'alex',correct:false}
+            ,{label:'james',correct:true}
+            ,{label:'peter',correct:false}
+          ]}
+      ]},
+      {Label:'quiz 2',id:'asdf2'},
+      {Label:'quiz 3',id:'asdf3'}];
+  });
+
+
+function ListCtrl($scope , quizData) {
+  $scope.quizes = quizData;
+}
+function TakeQuiz( $scope , quizData, $routeParams){
+  console.log( $routeParams );
+  $scope.quizes = quizData;
+
+}
