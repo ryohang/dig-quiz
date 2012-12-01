5fa239f918d6469ecacf699feb41565be84b24eb
Alex van <yell@pituki.com>
2012-12-01 11:26:34 -0500

    angular started and linked with test quiz list

--- html/js/project.js
+++ html/js/project.js
@@ -1,13 +1,16 @@
-angular.module('project').
+angular.module('project',[]).
   config(function($routeProvider) {
     $routeProvider.
       when('/', {controller:ListCtrl, templateUrl:'list.html'}).
-      when('/q/:projectId', {controller:EditCtrl, templateUrl:'detail.html'}).
+      when('/q/:projectId', {controller:TakeQuiz, templateUrl:'detail.html'}).
       //when('/new', {controller:CreateCtrl, templateUrl:'detail.html'}).
       otherwise({redirectTo:'/'});
   });
 
 
-function ListCtrl($scope, Project) {
+function ListCtrl($scope ) {
   $scope.quizes = ['quiz 1', 'quiz 2', 'quiz 3'];
+}
+function TakeQuiz( $scope ){
+
 }
