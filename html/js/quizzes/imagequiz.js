2e9b6e143fdd096a3c5aa6ab52cebbbaf8489349
Anthony Delorie <adelorie@firstviewfed.com>
2012-12-01 13:16:12 -0500

    added the three quizzes as json source files

---
+++ html/js/quizzes/imagequiz.js
@@ -0,0 +1,54 @@
+quiz = {
+    questions: [
+        {
+            question: "What is a subtractive color model that stands for Cyan, Magenta, Yellow, and Black?",
+            answer: "CMYK", 
+            noise: "RGB", "TWAIN", "CMS"
+        },
+        {
+            question: "What is an additive color model and stands for Red, Green, and Blue?",
+            answer: "RGB", 
+            noise: "CMYK", "ASCII", "DHTML"
+        },
+        {            
+            question: "The bit depth for four colors is always __ bits.", 
+            answer: "3",
+            noise: "2", "8", "4"
+        },
+        {           
+            question: "4,000 pixels x 2,000 pixels =  ____ Megapixels.",
+            answer: "8", 
+            noise: "800", "8000", "6"
+        },
+        {  
+            question: "An 8-bit color depth allows _____ colors.",
+            answer: "256",
+            noise: "8", "128", "1024"
+        },
+        {            
+            question: "Approximately, what will happen to the file size if the total number of pixels of an image is doubled?",
+            answer: "the file size doubles",
+            noise: ["the file size triples", "the file size will be half", "the file size will quadruple"]
+        },
+        {
+            question: "What does HSB stand for?",
+            answer: "Hue, Saturation, and Brightness",
+            noise: ["Hypertext, Sampling, and Bytes", "Hue, Sampling, and Boolean", "Histogram, Saturation, and Bitmap"]
+        },
+        {            
+            question: "The term ________ comes from abridging the words: compressor and decompressor.",
+            answer: "codec",
+            noise: ["chrominance", "complementary", "compilation"]
+        },
+        {
+            question: "The term pixel stands for picture _________.",
+            answer: "element",
+            noise: ["extension", "expression", "envelope"]
+        },
+        {
+            question: "A ___________ of an image is a bar chart that shows the relative number of pixels plotted against a color value.",
+            answer: "histogram", 
+            noise: ["histology", "hierarchy", "pixelation"]
+        }
+    ]
+};
