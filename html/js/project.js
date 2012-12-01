653360c9b29910e9da5b936a8a3ec2ac85dcbb6e
Alex van <yell@pituki.com>
2012-12-01 12:08:41 -0500

    added list of quizes. working on quiz detail page.

--- html/js/project.js
+++ html/js/project.js
@@ -2,15 +2,37 @@ angular.module('project',[]).
   config(function($routeProvider) {
     $routeProvider.
       when('/', {controller:ListCtrl, templateUrl:'list.html'}).
-      when('/q/:projectId', {controller:TakeQuiz, templateUrl:'detail.html'}).
+      when('/q/:quizId', {controller:TakeQuiz, templateUrl:'detail.html'}).
       //when('/new', {controller:CreateCtrl, templateUrl:'detail.html'}).
       otherwise({redirectTo:'/'});
+  }).service('quizData', function(){
+    return [
+      {Label:'quiz 1'
+        ,id:'asdf1'
+        ,questions:[
+        {label:"your name",
+          options:[
+            {label:'alex',correct:false}
+            ,{label:'james',correct:true}
+            ,{label:'peter',correct:false}
+          ]}
+        ,{label:"your name",
+          options:[
+            {label:'alex',correct:false}
+            ,{label:'james',correct:true}
+            ,{label:'peter',correct:false}
+          ]}
+      ]},
+      {Label:'quiz 2',id:'asdf2'},
+      {Label:'quiz 3',id:'asdf3'}];
   });
 
 
-function ListCtrl($scope ) {
-  $scope.quizes = ['quiz 1', 'quiz 2', 'quiz 3'];
+function ListCtrl($scope , quizData) {
+  $scope.quizes = quizData;
 }
-function TakeQuiz( $scope ){
+function TakeQuiz( $scope , quizData , quizId){
+  $scope.quizId = quizId;
+  $scope.quizes = quizData;
 
 }
